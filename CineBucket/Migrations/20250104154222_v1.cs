8ca4dfdc957cbff7ac13cfb617060a6ebecf8ee2
inacio88 <inacio88@outlook.com>
2025-01-04 12:44:23 -0300

    main: migration v1

---
+++ CineBucket/Migrations/20250104154222_v1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CineBucket.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteMovies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TmdbId = table.Column<int>(type: "integer", nullable: false),
+                    OriginalTitle = table.Column<string>(type: "NVARCHAR", maxLength: 65, nullable: false),
+                    PosterPath = table.Column<string>(type: "NVARCHAR", maxLength: 500, nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Runtime = table.Column<int>(type: "integer", nullable: true),
+                    Status = table.Column<string>(type: "NVARCHAR", maxLength: 50, nullable: true),
+                    Title = table.Column<string>(type: "NVARCHAR", maxLength: 60, nullable: false),
+                    Priority = table.Column<short>(type: "SMALLINT", nullable: false),
+                    AddedAt = table.Column<DateTime>(type: "DATETIME", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteMovies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteMovies");
+        }
+    }
+}
