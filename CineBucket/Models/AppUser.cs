9a5c68bbce4df897b03b89fb1dfaf8a0c8ff7d7c
inacio88 <inacio88@outlook.com>
2025-01-04 23:23:07 -0300

    implementacao-identity: modelo personal user

---
+++ CineBucket/Models/AppUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace CineBucket.Models;
+
+public class AppUser: IdentityUser
+{
+    [StringLength(100)]
+    [MaxLength(100)]
+    [Required]
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+}
