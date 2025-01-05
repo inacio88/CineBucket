8156ab1af8351aea555a3fc64fc8f823cc51d338
inacio88 <inacio88@outlook.com>
2025-01-05 19:04:02 -0300

    dev: sem uso de hsts

--- CineBucket/Program.cs
+++ CineBucket/Program.cs
@@ -17,9 +17,9 @@ var app = builder.Build();
 // // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 else{
     using var dbContext = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>().UseNpgsql(ConfiguracoesGerais.ConnectionString).Options);
